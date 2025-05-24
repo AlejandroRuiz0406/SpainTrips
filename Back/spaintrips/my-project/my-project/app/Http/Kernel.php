41c05d9fa0a886513f74596d791ee9373c1adce5
Alejandro Ruiz <loloresto04@gmail.com>
2025-05-24 13:40:04 +0200

    commit spaintrips final 2

--- Back/spaintrips/my-project/my-project/app/Http/Kernel.php
+++ Back/spaintrips/my-project/my-project/app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
         \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\CorsMiddleware::class,
     ];
 
     /**
